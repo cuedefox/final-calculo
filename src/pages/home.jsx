2ec7671a2d353b954feee2a525719f67e4fbf8f0
Rodrigo Benjamin Vergara <rodrigovergara2001@gmail.com>
2024-05-29 16:24:09 -0300

    add styles

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,11 +2,13 @@ import { Link } from "react-router-dom";
 
 const Home = () => {
     return <>
-        <div>
-            <h1>Funciones Graficas Quiz</h1>
-            <Link to={'/game'}>
-                <button>Comenzar</button>
-            </Link>
+        <div className="home-container">
+            <div>
+                <h1>Funciones Graficas Quiz</h1>
+                <Link to={'/game'}>
+                    <button className="button">Comenzar</button>
+                </Link>
+            </div>
         </div>
     </>
 }
