eaaae753e661c91e9e3098d915a808c776ad75cd
Rodrigo Benjamin Vergara <rodrigovergara2001@gmail.com>
2024-05-29 16:44:56 -0300

    add styles

---
+++ src/components/question.jsx
@@ -0,0 +1,9 @@
+const Question = ({img, question, questions}) => {
+    return <div className='question'>
+        <h4>Pregunta x/10</h4>
+        <h3>Pregunta</h3>
+        <img src="" alt="" />
+    </div>
+}
+
+export default Question;
