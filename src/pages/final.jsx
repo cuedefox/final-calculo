397f97a65663093c446f447d8c103bf4de727a09
Rodrigo Benjamin Vergara <rodrigovergara2001@gmail.com>
2024-05-29 15:36:53 -0300

    add router logic

--- src/pages/final.jsx
+++ src/pages/final.jsx
@@ -1,10 +1,16 @@
+import { Link } from "react-router-dom";
+
 const Final = () => {
     return <>
         <div>
             <h2>Puntuacion:</h2>
             <div>
-                <button>Jugar de nuevo</button>
-                <button>Volver al inicio</button>
+                <Link to='/game'>
+                    <button>Jugar de nuevo</button>
+                </Link>
+                <Link to='/'>
+                    <button>Volver al inicio</button>
+                </Link>
             </div>
         </div>
     </>
